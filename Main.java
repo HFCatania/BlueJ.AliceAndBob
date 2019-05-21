78aaadf84e5514936f339471dde8129015073d8a
Henry <cataniahf@gmail.com>
2019-05-21 18:12:47 -0400

    Finished program

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
-
-    }
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scnr.nextLine();
+        
+        if (name.equalsIgnoreCase("Alice")) {
+        System.out.print("Ahoyhoy Alice!");
+    } else if (name.equalsIgnoreCase("Bob")) {
+        System.out.print("Ahoyhoy Bob!");
+    } else { 
+        System.out.print("Piss off stranger!");
+    }      
+}
 }
